88e23688dfaff544a2b7f380ddabf95b557a9c75
mytkadev <mytkalol@gmail.com>
2025-06-02 00:37:39 -0300

    penultimos ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import scrollRight from './assets/scroll-right.png';
 import scrollLeft from './assets/scroll-left.png';
 import smartPlatform from './assets/smartplatform-img.png'
 import smartPlatformLogo from './assets/smartplatform-logo.png'
+import campImg from './assets/camp-img.png'
+import mailLogo from './assets/mail-icon.png'
+import wppLogo from './assets/wpp-icon.png'
+import smartagriLogo from './assets/logo-navbar.png'
 
 // Hook para acompanhar a largura da janela
 function useWindowWidth() {
@@ -25,6 +29,16 @@ const cards = [
   { title: 'IoT, Sensoriamento e Automação', desc: 'Proin eu pellentesque justo. Vestibulum dui turpis.', img: firstCard },
   { title: 'Visão computacional', desc: 'Proin eu pellentesque justo. Vestibulum dui turpis.' },
   { title: 'Geoprocessamento e Imagens', desc: 'Drone e satélite' }
+];
+
+const smartplatformCards = [
+{ title: 'SmartTalents', desc: 'Monitoramento e gestão de processos', icon:firstCard},
+  { title: 'SmartBI', desc: 'Monitoramento e gestão de equipamentos', icon:firstCard },
+  { title: 'SmartTools', desc: 'Monitoramento do clima',icon:firstCard },
+  { title: 'SmartMaps', desc: 'Gestão de águas e irrigacão', img: firstCard },
+  { title: 'SmartProcess', desc: 'Gestão de energia',icon:firstCard },
+  { title: 'SmartMetrics', desc: 'Gestão',icon:firstCard }
+  
 ];
 
 function App() {
@@ -59,24 +73,40 @@ function App() {
     return cards[index];
   });
 
+  const [scrolled, setScrolled] = useState(false);
+
+useEffect(() => {
+  const handleScroll = () => {
+    if (window.scrollY > 20) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+}, []);
+
   return (
     <>
 <div className=" bg-white text-black">
       {/* Navbar */}
-      <header className="bg-transparent shadow fixed top-0 left-0 w-full z-50">
+      <header className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${scrolled ? "backdrop-blur-sm bg-black/60" : "bg-transparent"}`}>
+
         <div className="max-w-full mx-auto px-6 md:px-20 h-16 flex items-center justify-between">
           <img src="src\assets\logo-navbar.png" alt="Logo SmartAgri" className="h-10 mr-2" />
           <nav className="hidden md:flex items-center gap-8 text-md font-medium text-white">
-            <span><a href="#" className="hover:text-green-600">Quem Somos</a></span>
-            <span><a href="#" className="hover:text-green-600">Soluções</a></span>
-            <span><a href="#" className="hover:text-green-600">SmartPlatform</a></span>
-            <span><a href="#" className="hover:text-green-600">Segmentos</a></span>
-            <span><a href="#" className="hover:text-green-600">Produtos</a></span>
-            <span><a href="#" className="hover:text-green-600">Parcerias</a></span>
-            <span><a href="#" className="hover:text-green-600">Cases</a></span>
-            <span><a href="#" className="hover:text-green-600">Contato</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">Quem Somos</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">Soluções</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">SmartPlatform</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">Segmentos</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">Produtos</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">Parcerias</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">Cases</a></span>
+            <span><a href="#" className="hover:text-[#58A91C]">Contato</a></span>
           </nav>
-          <button className="hidden md:block bg-white text-black px-4 py-2 rounded-sm text-md font-semibold hover:bg-green-600 transition">
+          <button className="hidden md:block bg-white text-black px-4 py-2 rounded-sm text-md font-semibold hover:bg-[#58A91C] transition">
             Ir para a SmartPlatform
           </button>
         </div>
@@ -84,12 +114,12 @@ function App() {
 
       <div className="relative"> 
 
-       <section className="relative h-[1500px] flex justify-center text-white overflow-hidden">
+       <section className="relative h-[900px] md:h-[1200px] flex justify-center text-white overflow-hidden">
 
   <img
     src="src/assets/bg-img.png"
     alt=""
-    className="absolute top-0 left-0 w-full h-full object-cover z-0"
+    className="absolute left-0 w-full h-full object-cover z-0"
   />
   
 
@@ -98,11 +128,11 @@ function App() {
       Soluções inovadoras para o agronegócio
     </h1>
     <div className="flex flex-col md:flex-row justify-center gap-4 items-center">
-          <button className=" flex items-center gap-3 border-[1px] border-white mt-4 md:mt-6 bg-transparent text-white px-4 py-2 rounded-lg text-md font-medium hover:bg-green-600 transition">
+          <button className=" flex items-center gap-3 border-[1px] border-white mt-4 md:mt-6 bg-black/20 backdrop-blur-md text-white px-4 py-2 rounded-lg text-md font-medium hover:bg-[#58A91C] transition">
             Conheça nossas soluções
             <img src="src\assets\arrow-down-hero.png" alt="" />
           </button>
-          <button className="flex items-center gap-3 md:mt-6 bg-white text-black px-4 py-2 rounded-lg text-md font-semibold hover:bg-green-600 transition">
+          <button className="flex items-center gap-3 md:mt-6 bg-white text-black px-4 py-2 rounded-lg text-md font-semibold hover:bg-[#58A91C] transition">
             Visitar a SmartPlatform
             <img src="src\assets\arrow-up-hero.png" alt="" />
           </button>
@@ -114,12 +144,12 @@ function App() {
   </div>
 
 </section>
-      <section className="px-9 xl:px-30 py-20">
+      <section className="px-6 xl:px-30 py-10">
         <div className="flex flex-col lg:flex-row justify-between"> 
               <div className="max-w-[800px]">        
-                <h2 className="uppercase text-sm roboto-condensed text-green-600 max-w-700px">Sobre a SmartAgri</h2>
+                <h2 className="uppercase text-sm roboto-condensed text-[#58A91C] max-w-700px">Sobre a SmartAgri</h2>
                 <h2 className="text-3xl md:text-6xl font-bold mb-4">Onde a tecnologia <br />encontra o agro</h2>
-                <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.
+                <p className="mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.
                 <br />
                 <br />
                 Aenean ultricies finibus diam, ut ultrices est scelerisque ut. In tempus bibendum mi tincidunt feugiat. Maecenas id nisi congue, scelerisque ante vel, hendrerit massa. In sed laoreet ligula. Phasellus quis sagittis velit. Ut consectetur ipsum libero, sed dictum risus malesuada eget. Quisque mollis arcu odio, ac rhoncus mauris ullamcorper id. Donec pharetra urna ac vestibulum feugiat.
@@ -131,16 +161,16 @@ function App() {
                 <img src="src\assets\Rectangle.png" alt="" className=""/>
                 </div> 
         </section>
-<section className="px-9 xl:px-15 py-20">
+<section className="px-6 xl:px-15 py-10">
   <div className="flex flex-col xl:flex-row justify-between xl:px-15 gap-2 xl:gap-20">
-    <div className="xl:max-w-[650px] xl:min-w-[550px]">    <h2 className="uppercase text-sm roboto-condensed text-green-600 max-w-700px">
+    <div className="xl:max-w-[650px] xl:min-w-[550px]">    <h2 className="uppercase text-sm roboto-condensed text-[#58A91C] max-w-700px">
       Conheça nossas soluções
     </h2>
     <h3 className="text-3xl md:text-6xl font-bold mb-4">
       Tecnologia para plantar resultados.
     </h3></div>
 
-    <p className="xl:max-w-[650px] xl:text-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. </p>
+    <p className="xl:max-w-[500px] xl:text-right text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. </p>
   </div>
 
 <div className="relative mt-10 ">
@@ -199,17 +229,28 @@ function App() {
   )}
 </div>
 </section>
-<section className=" py-20">
-        <div className=" pl-9 xl:pl-30 flex flex-col justify-between max-w-[900px]">    
-                <h2 className="uppercase text-sm roboto-condensed text-green-600 max-w-700px">Nossos Produtos</h2>
+<section className=" py-10">
+        <div className=" px-6 xl:pl-30 xl:pr-0 flex flex-col justify-between max-w-[900px]">    
+                <h2 className="uppercase text-sm roboto-condensed text-[#58A91C] max-w-700px">Nossos Produtos</h2>
                 <h2 className="text-3xl md:text-6xl font-bold ">Ferramentas que transformam a agricultura</h2>
                 </div > 
-                <div className="flex">
-                <img src={smartPlatform} alt="" className=" w-full max-w-[700px] xl:ml-[-20px]"/>
-                <div className="flex flex-col">
-                  <img src={smartPlatformLogo} alt="" />
-                  <h4>Tenha controle de tudo em <span>uma única plataforma</span></h4>
+                <div className="flex flex-col xl:flex-row">
+                <img src={smartPlatform} alt="" className=" w-full max-w-[900px] xl:ml-[-20px] mb-5 xl:mb-0"/>
+                <div className="flex flex-col px-6 mt-5 md:mt-0 xl:pr-30 align-center justify-center gap-4">
+                  <div className="flex flex-col gap-2">
+                    <img src={smartPlatformLogo} alt="" className="w-[120px] md:w-[200px]"/>
+                  <h4 className="text-2xl md:text-6xl font-semibold max-w-[500px]">Tenha controle de tudo em <span className=" italic font-black text-[#58A91C] ">uma única plataforma</span></h4>
+                  
+                  </div>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.</p>
+<div className="grid grid-cols-2 gap-2 text-sm text-white">
+  {smartplatformCards.map((item, i) => (
+    <div key={i} className="bg-black px-3 py-2 rounded flex flex-col">
+      <span className="text-md font-semibold">{item.title}</span>
+      <span className="text-sm">{item.desc}</span></div>
+  ))}
+</div>
+
                 </div>
                 </div>
         </section>
@@ -220,34 +261,50 @@ function App() {
 
 
       {/* Para cada campo, uma solução */}
-      <section className="px-6 md:px-20 py-20">
-        <h2 className="text-2xl md:text-3xl font-bold mb-2">Para cada campo,<br />uma solução</h2>
-        <p className="text-gray-700 max-w-2xl mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 text-sm text-gray-700">
+      <section className="px-6 xl:px-30 py-10">
+                        <div className="flex bg-red flex-col xl:flex-row justify-between items-center">
+        <div className="flex flex-col  justify-between xl:max-w-[900px]">    
+                            <div>
+                <h2 className="uppercase text-sm roboto-condensed text-[#58A91C] max-w-700px">Onde estamos presentes</h2>
+                <h2 className="text-3xl md:text-6xl font-bold max-w-[600px] mb-4">Para cada campo, uma solução</h2>
+                </div > 
+
+
+        <p className="text-gray-700 xl:max-w-2xl mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.</p>
+        <div className=" max-w-2xl grid grid-cols-2 md:grid-cols-4 gap-4 text-sm text-gray-700">
           {['Alcool e açúcar', 'Irrigação', 'Madeira', 'Arroz', 'Leite e derivados', 'Soja', 'Eucalipto'].map((segment, i) => (
             <div key={i} className="flex items-center gap-2">
-              <span className="w-2 h-2 rounded-full bg-green-600"></span>
+              <span className="w-3 h-3 rounded-full bg-[#58A91C]"></span>
               {segment}
             </div>
           ))}
+          
         </div>
+                </div>
+        <img src={campImg} alt="" className="mt-4 xl:mt-0 w-fit xl:w-[500px] md:ml-6 h-auto object-contain"/>
+        </div>
+
       </section>
 
       {/* Cases */}
-      <section className="bg-green-600 px-6 md:px-20 py-20 text-white">
-        <h2 className="text-2xl md:text-3xl font-bold mb-8">Transformações que cultivamos juntos.</h2>
-        <div className="bg-white text-black p-6 rounded-md shadow-md">
-          <h3 className="font-bold mb-2">Título</h3>
-          <p className="text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+      <section className="">
+        <div className=" px-6 xl:px-30 flex flex-col justify-between max-w-[900px]">    
+                <h2 className="uppercase text-sm roboto-condensed text-[#58A91C] max-w-700px">Cases</h2>
+                <h2 className="text-3xl md:text-6xl font-bold ">Transformações que cultivamos juntos.</h2>
+                </div > 
+        <div className="bg-black text-white mx-6 xl:mx-30 my-8 h-[300px] md:h-[500px] p-6 md:p-10 rounded-md shadow-md flex flex-col justify-between">
+          <div>          <h3 className="font-bold mb-2">Título</h3>
+          <p className="text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p></div>
           <div className="flex justify-between items-center">
             <div className="flex items-center gap-2">
-              <span className="w-5 h-5 rounded-full bg-black inline-block"></span>
+              <span className="w-8 h-8 rounded-full bg-white inline-block"></span>
               <div>
                 <p className="text-sm font-medium">Nome da pessoa</p>
                 <p className="text-xs">Cargo da pessoa</p>
               </div>
             </div>
-            <p className="text-4xl font-bold text-gray-300">99</p>
           </div>
         </div>
       </section>
@@ -255,12 +312,29 @@ function App() {
       </div>
 
       {/* Footer */}
-      <footer className="bg-gray-900 text-white text-center py-10">
-        <img src="/path/to/logo.png" alt="Logo" className="mx-auto mb-4 w-12" />
-        <p className="text-xl font-bold">Entre em contato</p>
-        <p className="mt-2 text-sm">contato@smartagri.com.br<br />+55 11 98604-9004</p>
+      <footer className="bg-[#58A91C] text-white text-center py-10">
+          <div className="flex justify-between h-6 px-6 md:px-30">
+            <img src={smartagriLogo} alt="" className=""/>
+            <div className="flex justify-center align-center gap-2">
+              <img src={mailLogo} alt="" />
+              <img src={wppLogo} alt="" />
+            </div>
+          </div>
+          <div className="px-6 md:px-30">
+          <div className="w-full h-px bg-white/40 mt-3 "></div>
+          </div>
+          <div className="mt-3 flex justify-between px-6 md:px-30">
+
+            <p className="text-xs font-light">2024 Smart Agri</p>
+          <div className="flex gap-2">
+            <a href="" className="underline text-xs font-light">Termos de Serviço</a>
+            <a href="" className="underline text-xs font-light">Política de Privacidade</a>
+          </div>
+          </div>
+          
       </footer>
     </div>
+
     </>
   )
 }
