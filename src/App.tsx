f432ddb2d0ffed6ffc7af072e5a2b1168a0f938b
mytkadev <mytkalol@gmail.com>
2025-06-26 22:28:34 -0300

    inserção de imagens e textos

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ import smartagriLogo from './assets/logo-navbar.png'
 import bgImg from './assets/bg-img.png'
 import arrowupHero from './assets/arrow-up-hero.png'
 import arrowdownHero from './assets/arrow-down-hero.png'
+import sixthCard from './assets/sixth-card.png'
+import fifthCard from './assets/fifth-card.png'
+import fourthCard from './assets/fourth-card.png'
+import secondCard from './assets/second-card.png'
+import thirdCard from './assets/third-card.png'
+
 
 // Hook para acompanhar a largura da janela
 function useWindowWidth() {
@@ -27,11 +33,11 @@ function useWindowWidth() {
 
 const cards = [
   { title: 'SmartPlatform', desc: 'Gestão centralizada e intuitiva.', img: firstCard },
-  { title: 'Gestão do agronegócio', desc: 'Visão ampla e gestão eficiente.' },
-  { title: 'Projetos customizados', desc: 'Tecnologia sob medida.' },
-  { title: 'IoT, Sensoriamento e Automação', desc: 'Proin eu pellentesque justo. Vestibulum dui turpis.', img: firstCard },
-  { title: 'Visão computacional', desc: 'Proin eu pellentesque justo. Vestibulum dui turpis.' },
-  { title: 'Geoprocessamento e Imagens', desc: 'Drone e satélite' }
+  { title: 'Gestão do agronegócio', desc: 'Visão ampla e gestão eficiente.', img: secondCard},
+  { title: 'Projetos customizados', desc: 'Tecnologia sob medida.', img: thirdCard },
+  { title: 'IoT, Sensoriamento e Automação', desc: 'Automação que gera eficiência.', img: fourthCard},
+  { title: 'Visão computacional', desc: 'Análise', img: fifthCard },
+  { title: 'Geoprocessamento e Imagens', desc: 'Drone e satélite', img: sixthCard }
 ];
 
 const smartplatformCards = [
@@ -198,14 +204,14 @@ const scrollToSection = (id: string) => {
               <div className="max-w-[800px]">        
                 <h2 className="uppercase text-sm roboto-condensed text-[#58A91C] max-w-700px">Sobre a SmartAgri</h2>
                 <h2 className="text-3xl md:text-6xl font-bold mb-4">Onde a tecnologia <br />encontra o agro</h2>
-                <p className="mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.
-                <br />
-                <br />
-                Aenean ultricies finibus diam, ut ultrices est scelerisque ut. In tempus bibendum mi tincidunt feugiat. Maecenas id nisi congue, scelerisque ante vel, hendrerit massa. In sed laoreet ligula. Phasellus quis sagittis velit. Ut consectetur ipsum libero, sed dictum risus malesuada eget. Quisque mollis arcu odio, ac rhoncus mauris ullamcorper id. Donec pharetra urna ac vestibulum feugiat.
-                <br />
-                <br />
-                Ut lobortis leo sit amet orci bibendum, vel sagittis ante sollicitudin. Integer ante ipsum, condimentum eget ante tempus, porttitor vulputate lorem. Quisque feugiat orci sem, ac tincidunt leo molestie et. Nunc ante nunc, efficitur vel turpis at, malesuada egestas lectus. Mauris luctus tempor nisi eget sodales.
-                </p>
+                <p>A Smart Agri é uma empresa de tecnologia que nasceu com o propósito de transformar a maneira como o agronegócio opera, levando inovação para o campo de forma acessível, inteligente e personalizada. Nosso foco está em desenvolver soluções que unam automação, sensoriamento, conectividade e gestão digital, sempre com o objetivo de tornar o dia a dia de agricultores, cooperativas e trabalhadores do setor mais eficiente, sustentável e conectado com o futuro.</p>
+
+                <p>Acreditamos que a tecnologia deve ser uma aliada prática e funcional — não um obstáculo. Por isso, criamos plataformas, sistemas e projetos que descomplicam a gestão agrícola, facilitam a tomada de decisões e aumentam a produtividade, sem perder de vista a realidade de quem está no campo. Nossas soluções são pensadas para integrar dados, processos e pessoas, colocando o controle da operação nas mãos de quem realmente precisa dele.</p>
+
+                <p>Um exemplo disso é o projeto que estamos desenvolvendo para o setor madeireiro: um sistema inteligente de corte de tábuas que automatiza o processo e reduz drasticamente o desperdício de matéria-prima. Antes, a empresa utilizava uma máquina rudimentar que exigia muito esforço manual e resultava em perda considerável de produto. Com a tecnologia Smart Agri, esse processo está sendo transformado, tornando-se mais preciso, rápido e eficiente.</p>
+
+                <p className="mb-6">Na Smart Agri, unimos inovação, experiência e sensibilidade com o campo para criar um futuro onde a tecnologia trabalha lado a lado com quem alimenta o mundo.</p>
+                
                 </div>
                 <img src="src\assets\Rectangle.png" alt="" className=""/>
                 </div> 
@@ -219,7 +225,7 @@ const scrollToSection = (id: string) => {
       Tecnologia para plantar resultados.
     </h3></div>
 
-    <p className="xl:max-w-[500px] xl:text-right text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. </p>
+    <p className="xl:max-w-[500px] xl:text-right text-xs">Desenvolvemos soluções tecnológicas que conectam automação, dados e simplicidade para transformar a rotina de quem vive do campo. Nosso objetivo é oferecer ferramentas que otimizem processos, reduzam desperdícios e facilitem decisões — seja no controle de máquinas, no monitoramento climático ou na produção agrícola. Com isso, construímos um agronegócio mais eficiente, sustentável e acessível para todos os envolvidos.</p>
   </div>
 
 <div className="relative mt-10 ">
@@ -291,7 +297,7 @@ const scrollToSection = (id: string) => {
                   <h4 className="text-2xl md:text-6xl font-semibold max-w-[500px]">Tenha controle de tudo em <span className=" italic font-black text-[#58A91C] ">uma única plataforma</span></h4>
                   
                   </div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.</p>
+                  <p>A SmartPlatform é o ecossistema digital que centraliza todas as soluções da Smart Agri em um só lugar. É por meio dela que o usuário monitora, gerencia e acompanha os principais indicadores de seus produtos, processos e equipamentos agrícolas. Com módulos como Smartplants, Smartmachines, Smartclimate, Smartwater, Smartenergy e o Smartagri MRP 4.0, é possível controlar desde a irrigação até o desempenho energético, o clima e a operação de máquinas em tempo real. A SmartPlatform transforma dados em decisões — com acessibilidade, clareza e foco em produtividade no campo.</p>
 <div className="grid grid-cols-2 gap-2 text-sm text-white">
   {smartplatformCards.map((item, i) => (
     <div key={i} className="bg-black px-3 py-2 rounded flex flex-col">
@@ -313,9 +319,11 @@ const scrollToSection = (id: string) => {
                 </div > 
 
 
-        <p className="text-gray-700 xl:max-w-2xl mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac pharetra magna, nec aliquet mi. Proin dictum ipsum sit amet mi tempor, non fermentum mauris ultrices. Sed lacinia accumsan enim, eu placerat lectus pellentesque eget. Fusce vel rhoncus tortor, et bibendum sapien. Aenean nec urna commodo, iaculis tortor ac, egestas quam. Praesent leo dui, convallis vitae dapibus et, euismod nec nibh. Vivamus porttitor, erat ac scelerisque consectetur, nulla tellus luctus dolor, et imperdiet nulla lectus at ante.</p>
+        <p className="text-gray-700 xl:max-w-2xl mb-6">O agronegócio é formado por diversos segmentos, cada um com suas particularidades, ritmos e desafios. Na Smart Agri, acreditamos que não existe uma solução única para todos — por isso, nossa proposta é desenvolver tecnologias sob medida para diferentes realidades do campo. Atuamos com uma visão flexível, buscando entender a fundo os processos e adaptar nossas ferramentas de forma prática, acessível e funcional.
+        Hoje, já trabalhamos com soluções voltadas para o setor madeireiro, como um sistema que automatiza o corte de tábuas com o objetivo de reduzir perdas e o esforço manual.
+        Além disso, temos projetos em fase de estudo e estruturação para outras áreas como Álcool e Açúcar, Irrigação, Arroz, Leite e Derivados, Soja e Eucalipto. Cada um desses setores está sendo analisado com atenção para que as futuras soluções desenvolvidas sejam de fato aplicáveis, eficientes e alinhadas à rotina de quem vive do campo.
+        Nosso objetivo é criar ferramentas que não apenas automatizem processos, mas que deem suporte à tomada de decisão, tornem o trabalho mais eficiente e promovam o crescimento sustentável em cada setor onde atuamos.
+        Cada campo tem sua lógica e seu jeito de produzir — e é com esse olhar que a Smart Agri constrói, passo a passo, soluções alinhadas à realidade de cada produtor e negócio rural.</p>
         <div className=" max-w-2xl grid grid-cols-2 md:grid-cols-4 gap-4 text-sm text-gray-700">
           {['Alcool e açúcar', 'Irrigação', 'Madeira', 'Arroz', 'Leite e derivados', 'Soja', 'Eucalipto'].map((segment, i) => (
             <div key={i} className="flex items-center gap-2">
